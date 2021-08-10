551525b710976900a923281c5e4c06acc4791600
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-08-10 22:10:32 +0200

    multiplevariables at once

--- helloworld.go
+++ helloworld.go
@@ -1,6 +1,7 @@
 package main
-import ("fmt")
+
+import "fmt"
 
 func main() {
-  fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
 }
---
+++ multiplevariable.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b int = 1, 2
+	fmt.Println(a, b)
+}
