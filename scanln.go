a94361446da9855dbaac0909375006be923faf54
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-08-10 23:00:19 +0200

    Added comments

--- scanln.go
+++ scanln.go
@@ -1,12 +1,18 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+)
 
-func main(){
+func main() {
 
 	var a, b string
 
 	fmt.Print("Type 2 strings: ")
 	fmt.Scan(&a, &b)
 	fmt.Println("Your strings are: ", a, "and", b)
+	/* Format needs to be
+	Line1
+	Line2
+	*/
 }
