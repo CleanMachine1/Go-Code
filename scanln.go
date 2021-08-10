f9cfc7b9aacf28efe001adba6f3cfe383a4973c2
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-08-10 23:20:56 +0200

    Fixed incorrect comment

--- scanln.go
+++ scanln.go
@@ -9,10 +9,9 @@ func main() {
 	var a, b string
 
 	fmt.Print("Type 2 strings: ")
-	fmt.Scan(&a, &b)
+	fmt.Scanln(&a, &b)
 	fmt.Println("Your strings are: ", a, "and", b)
 	/* Format needs to be
-	Line1
-	Line2
+	String1 String2
 	*/
 }
