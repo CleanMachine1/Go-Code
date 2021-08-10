2b5cf868618b443b201a9e549d64e923dc3915a2
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-08-10 21:56:48 +0200

    Using variables

---
+++ variable.go
@@ -0,0 +1,12 @@
+package main
+
+import ("fmt")
+
+func main() {
+	var message1 = 1
+	var message2 = 1
+	var message3 = message1 + message2
+	fmt.Println(message1)
+	fmt.Println(message2)
+	fmt.Println(message3)
+}
