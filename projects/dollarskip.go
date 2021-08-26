9c4bb20d1a71261591af3a9c8b5ac73a56079824
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-08-26 17:57:31 +0200

    general changes

--- projects/dollarskip.go
+++ projects/dollarskip.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"os/exec"
-	"strings"
+	"fmt"     // for printing the lines
+	"log"     // error related
+	"os"      // for capturing argv
+	"os/exec" // for running the commands
+	"strings" // editing the strings such as removing square brackets and adding spaces
 )
 
 func main() {
 	input := os.Args[1:]                  // collects given arguments and 1: removes the binary name
 	var1 := strings.Join(input, " ")      // convert to string rather than type string[] from args
-	var2 := strings.TrimPrefix(var1, "[") // Remove square brackets from using 1: earlier
-	var3 := strings.Trim(var2, "]")       // Read above ^
+	var2 := strings.TrimSuffix(var1, "[") // Remove square brackets from using 1: earlier
+	var3 := strings.TrimPrefix(var2, "]") // Read above ^
 
-	cmd, err := exec.Command(`bash`, `-c`, var3).Output() // 
+	cmd, err := exec.Command(`bash`, `-c`, var3).Output() // execute the command
 
-	if err != nil {
-		log.Fatal(err)
+	if err != nil { // error capturing
+		log.Fatal(err) // report time and error status code
 	}
 
-	output := string(cmd)
-	fmt.Println(output)
+	output := string(cmd) // Converting the byte[] to string
+	fmt.Println(output)   // print what the command outputted
 }
