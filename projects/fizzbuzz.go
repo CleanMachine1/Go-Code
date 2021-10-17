7570aa1556d78d5e44221d19fc6404ec010bba32
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-10-17 18:40:30 +0200

    fizzbuzz

---
+++ projects/fizzbuzz.go
@@ -0,0 +1,20 @@
+// Basic FizzBuzz program
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	for i = 0; i <= 100; i++ {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Printf("%d\n", i)
+		}
+	}
+
+}
