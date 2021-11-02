c49d1157517b832ba5985f77a0f5ca415c3f2970
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-11-02 14:13:54 +0100

    Comments

--- file_append.go
+++ file_append.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 	file, err := os.OpenFile("testdata.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755) // Open file, append or create and give 755 perms
-	if err != nil {
+	if err != nil { // Error checking
 		return
 	}
-	defer file.Close()
+	defer file.Close() // When the function is complete this will run
 
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
