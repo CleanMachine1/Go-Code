c80add6e482b3f8e9699f5f0136f37e811b3a2ae
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-11-07 00:53:11 +0100

    Added a multiples of _

---
+++ timestables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var number, i int // The number being multiplied by
+	fmt.Scanf("%d", &number)
+	for i = 1; i <= 10; i++ { // Starts at x1, goes to 10, repeats the timing ten times
+		output := i * number
+		fmt.Println(output) // Print the total for each run
+
+	}
+
+}
