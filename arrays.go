95fb6a514af736956cb07577f09899784d3ebda7
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-11-07 00:24:03 +0100

    array edits

--- arrays.go
+++ arrays.go
@@ -1,15 +1,16 @@
 package main
 
 import "fmt"
+
 // Arrays can not be resized
 func main() {
 	var a [2]string
 	a[0] = "Hello"
 	a[1] = "World"
 	fmt.Println(a[0], a[1]) // Hello World
-	fmt.Println(a) // [Hello World]
+	fmt.Println(a)          // [Hello World]
 
 	primes := [6]int{2, 3, 5, 7, 11, 13}
-	fmt.Println(primes) // [2 3 5 7 11 13]
-	fmt.Println(primes[0]) // 2 
+	fmt.Println(primes)    // [2 3 5 7 11 13]
+	fmt.Println(primes[0]) // 2
 }
